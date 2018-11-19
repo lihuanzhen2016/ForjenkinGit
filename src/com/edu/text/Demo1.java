0ef629c8bf1201115c90d336c1251ebdc2e67de4
lihuanzhen2016 <lihuanzhen@onest.net>
2018-11-19 15:53:41 +0800

    111

--- src/com/edu/text/Demo1.java
+++ src/com/edu/text/Demo1.java
@@ -3,7 +3,7 @@ package com.edu.text;
 public class Demo1 {
 
 	public static void main(String[] args) {
-		System.out.println("he23111«Î«Û111232");
+		System.out.println("he2322111«Î«Û111232");
 
 	}
 
